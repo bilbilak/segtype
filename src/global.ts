f5026ed8e9eef297f60824c7835ad41b50fca6c9
Sam Ariafar <me@samariafar.com>
2026-06-09 00:30:02 +0200

    ✨ feat: add module entry points and charmap variant API

---
+++ src/global.ts
@@ -0,0 +1,5 @@
+import { defineSeg } from './seg';
+import { THEMES } from './themes';
+defineSeg();
+export { SegDisplay, defineSeg } from './seg';
+export { THEMES } from './themes';
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { SegDisplay, defineSeg } from './seg';
+export { getSegments, getCharVariants } from './charmap';
+export { MODES } from './geometry';
+export type { SegmentSet } from './geometry';
+export { THEMES } from './themes';
+export type { ThemeVars } from './themes';
